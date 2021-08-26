a8b859004aa95a2f38bb2700ebadda432f1278a6
Acherons_ <kamron.cole13@gmail.com>
2021-08-26 01:08:29 -0400

    git version check, auto finds default branch, skips repos with no commits, only logs errors, added colors!

--- cloneRepositories.py
+++ cloneRepositories.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import csv
 import logging
 import os
+import re
 import shutil
 import subprocess
 import _thread
@@ -18,16 +19,22 @@ Script to clone all or some repositories in a Github Organization based on repo 
 """
 CONFIG_PATH = 'tmp/config.txt' # Stores token and org name
 BASE_GITHUB_LINK = 'https://github.com'
+MIN_GIT_VERSION = 2.30
+MAX_THREADS = 200
+LOG_FILE_PATH = 'tmp/logs.log'
+RED = '\033[0;31m'
+WHITE = '\033[0m'
+LIGHT_GREEN = '\033[1;32m'
 
 
 '''
 The Thread that clones the repo
 '''
 class CloneRepoThread(Thread):
-    __slots___ = ['__repo', '__assignment_name', '__date_due', '__time_due', '__students', '__student_filename', '__initial_path', '__default_branch']
+    __slots___ = ['__repo', '__assignment_name', '__date_due', '__time_due', '__students', '__student_filename', '__initial_path']
 
 
-    def __init__(self, repo, assignment_name, date_due, time_due, students, student_filename, initial_path, default_branch):
+    def __init__(self, repo, assignment_name, date_due, time_due, students, student_filename, initial_path):
         self.__repo = repo
         self.__assignment_name = assignment_name
         self.__date_due = date_due
@@ -35,7 +42,6 @@ class CloneRepoThread(Thread):
         self.__students = students
         self.__student_filename = student_filename
         self.__initial_path = initial_path
-        self.__default_branch = default_branch
         super().__init__()
 
 
@@ -44,9 +50,9 @@ class CloneRepoThread(Thread):
     '''
     def run(self):
         try:
-            clone_repo(self.__repo, self.__assignment_name, self.__date_due, self.__time_due, self.__students, self.__student_filename, self.__initial_path, self.__default_branch)
+            clone_repo(self.__repo, self.__assignment_name, self.__date_due, self.__time_due, self.__students, self.__student_filename, self.__initial_path)
         except: # Catch exception raised by clone_repo and interrupt main thread
-            print(f'ERROR: Sorry, ran into a problem while cloning `{self.__repo.name}`. Check tmp/logs.log.')
+            print(f'ERROR: Sorry, ran into a problem while cloning `{self.__repo.name}`. Check {LOG_FILE_PATH}.')
             logging.exception('ERROR:')
             _thread.interrupt_main()            
 
@@ -83,7 +89,7 @@ def get_students(student_filename: str) -> dict:
                 if name and github:
                     students[github] = name
     else:
-        raise FileNotFoundError()
+        raise FileNotFoundError(f'Classroom roster file `{student_filename}` not found.')
     return students
 
 
@@ -127,7 +133,7 @@ def file_exists_handler(path):
             shutil.rmtree(path)
             Path.mkdir(path)
         except:
-            raise FileExistsError()
+            raise FileExistsError('Assignment file already exists. Delete and re-run script.')
     else:
         Path.mkdir(path)
 
@@ -135,7 +141,7 @@ def file_exists_handler(path):
 '''
 Save parameters into config file to be read on future runs
 '''
-def save_config(token: str, organization: Organization, use_classlist: bool, student_filename: str, default_branch: str):
+def save_config(token: str, organization: Organization, use_classlist: bool, student_filename: str):
     with open(CONFIG_PATH, 'w') as config:
         config.write(token)
         config.write('\n')
@@ -144,8 +150,6 @@ def save_config(token: str, organization: Organization, use_classlist: bool, stu
         config.write(str(use_classlist))
         config.write('\n')
         config.write(student_filename)
-        config.write('\n')
-        config.write(default_branch)
 
 
 '''
@@ -157,7 +161,6 @@ def read_config_raw() -> tuple:
     organization = ''
     use_classlist = ''
     student_filename = ''
-    default_branch = ''
     if opener(CONFIG_PATH):
         with open(CONFIG_PATH, 'r') as config:
             token = config.readline().strip()
@@ -168,8 +171,7 @@ def read_config_raw() -> tuple:
                 student_filename = config.readline().strip()
             elif use_classlist == 'False':
                 use_classlist = False
-            default_branch = config.readline().strip()
-    return (token, organization, use_classlist, student_filename, default_branch)
+    return (token, organization, use_classlist, student_filename)
 
 
 '''
@@ -177,7 +179,7 @@ Checks whether config already exists, if so and use_classlist is False, ask for 
 '''
 def read_config() -> tuple:
     if opener(CONFIG_PATH): # If config already exists
-        token, organization, use_classlist, student_filename, default_branch = read_config_raw() # get variables
+        token, organization, use_classlist, student_filename = read_config_raw() # get variables
         if use_classlist == False:
             print('OPTIONAL: Enter filename of csv file containing username and name of students. To ignore, just hit `enter`')
             student_filename = input('If ignored, repo names will not be changed to match student names: ')
@@ -190,11 +192,11 @@ def read_config() -> tuple:
                     use_classlist = 'False'
             else:
                 use_classlist = 'False'
-            save_config(token, organization, use_classlist, student_filename, default_branch)
+            save_config(token, organization, use_classlist, student_filename)
     else:
         make_default_config()
-        token, organization, use_classlist, student_filename, default_branch = read_config_raw() # Update return variables
-    return (token, organization, student_filename, default_branch)
+        token, organization, use_classlist, student_filename = read_config_raw() # Update return variables
+    return (token, organization, student_filename)
 
 
 '''
@@ -203,7 +205,6 @@ Creates a default config file getting access token, org, class roster, etc
 def make_default_config():
     use_classlist = ''
     student_filename = ''
-    default_branch = ''
     token = input('Github Authentication Token: ')
     organization = input('Organization Name: ')
     print('OPTIONAL: Enter filename of csv file containing username and name of students. To ignore, just hit `enter`')
@@ -217,35 +218,47 @@ def make_default_config():
             use_classlist = 'False'
     else:
         use_classlist = 'False'
-    default_branch = input('Default Branch Name (can be changed later in tmp/config.txt): ')
-    save_config(token, organization, use_classlist, student_filename, default_branch)
+    save_config(token, organization, use_classlist, student_filename)
 
 
 '''
 Clones a repo into the assignment folder.
 If a classroom roster is used, replace github names with real names
 '''
-def clone_repo(repo, assignment_name, date_due, time_due, students, use_students, initial_path, default_branch):
+def clone_repo(repo, assignment_name, date_due, time_due, students, use_students, initial_path):
     if use_students: # If student file was input
         path = initial_path / get_new_repo_name(repo, students, assignment_name) # replace repo name when cloning to have student's real name
     else:
         path = initial_path / repo.name
+    
+    # If no commits, skip repo
+    try:
+        num_commits = len(list(repo.get_commits()))
+    except:
+        print(f'{RED}Skipping {repo.name}. It has 0 commits.{WHITE}')
+        logging.warning(f'Skipping repo `{repo.name}` because it has 0 commits.')
+        return
+
     # Clone repo
     print(f'Cloning {repo.name} into {path}...')
     subprocess.run(f'git clone {repo.clone_url} "{str(path)}"', stderr=subprocess.PIPE) # git clone to output file, Hides output from console
-    # Get commit hash at timestamp and reset local repo to timestamp (git rev-list output piped to git checkout)
-    rev_list_process = subprocess.Popen(f'git rev-list -n 1 --before="{date_due.strip()} {time_due.strip()}" origin/{default_branch} | git checkout', cwd=path, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    with rev_list_process: # Output command responds to log file
+    # Get commit hash at timestamp and reset local repo to timestamp on the default branch (git rev-list output piped to git checkout)
+    rev_list_process = subprocess.Popen(f'git rev-list -n 1 --before="{date_due.strip()} {time_due.strip()}" origin/{repo.default_branch} | git checkout', cwd=path, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    with rev_list_process: # Output command response to log file
         for line in iter(rev_list_process.stdout.readline, b''): # b'\n'-separated lines
             line = str(line)
             if 'fatal' in line.lower() or 'error' in line.lower(): # if git command threw error (usually wrong branch name)
                 logging.info('Subprocess: %r', line) # Log error
-                raise Exception("ERROR: An error has occured, check tmp/logs.log") # Raise exception to the thread
-    exit_code = rev_list_process.wait()
+                raise Exception(f'An error has with git.') # Raise exception to the thread
 
-    # if process didn't exit successfully
-    if exit_code != 0:
-        raise Exception(f'Exit Code: {exit_code}')
+
+'''
+Check that git version is above min requirements for script
+'''
+def check_git_version():
+    git_version = str(subprocess.check_output('git --version', stderr=subprocess.PIPE))[14:18]
+    if float(git_version) < MIN_GIT_VERSION:
+        raise ValueError('Incompatible git version.')
 
 
 '''
@@ -253,15 +266,17 @@ Main function
 '''
 def main():
     # Create log file
-    logging.basicConfig(level=logging.INFO, filename='tmp/logs.log')
+    logging.basicConfig(level=logging.INFO, filename=LOG_FILE_PATH)
 
     # Try catch catches errors and sends them to the log file instead of outputting to console
     try:
+        # Check local git version is compatible with script
+        check_git_version()
         # Read config file, if doesn't exist make one using user input.
-        token, organization, student_filename, default_branch = read_config()
+        token, organization, student_filename = read_config()
 
         # Create Organization to access repos
-        git_org_client = Github(token.strip()).get_organization(organization.strip())
+        git_org_client = Github(token.strip(), pool_size = MAX_THREADS).get_organization(organization.strip())
 
         # Variables used to get proper repos
         assignment_name = input('Assignment Name: ')
@@ -278,6 +293,7 @@ def main():
         print()
 
         # If student roster is specified, get repos list using proper function
+        students = dict()
         if student_filename:
             students = get_students(student_filename)
             repos = get_repos_specified_students(assignment_name, git_org_client, students)
@@ -294,9 +310,8 @@ def main():
         # goes through list of repos and clones them into the assignment's parent folder
         for repo in repos:
             # Create thread that clones repo and add to thread list
-            thread = CloneRepoThread(repo, assignment_name, date_due, time_due, students, bool(student_filename), initial_path, default_branch)
+            thread = CloneRepoThread(repo, assignment_name, date_due, time_due, students, bool(student_filename), initial_path)
             thread.setDaemon(True)
-            # thread = threading.Thread(target=clone_repo, args=(repo, assignment_name, date_due, time_due, students, bool(student_filename), initial_path, default_branch), daemon=True)
             threads += [thread]
 
         # Run all clone threads
@@ -307,19 +322,27 @@ def main():
             thread.join()
 
         print()
-        print(f'Done.')
-        print(f'Cloned {len(repos)} repo(s) for assignment `{assignment_name}`')
-    except FileNotFoundError:
+        print(f'{LIGHT_GREEN}Done.{WHITE}')
+        print(f'{LIGHT_GREEN}Cloned {len(next(os.walk(initial_path))[1])}/{len(repos)} repos.{WHITE}')
+    except FileNotFoundError as e:
         print()
         print(f'Classroom roster `{student_filename}` not found.')
-    except FileExistsError: # Error thrown if parent assignment file already exists
+        logging.error(e)
+    except FileExistsError as e: # Error thrown if parent assignment file already exists
         print()
         print(f'ERROR: File `{initial_path}` already exists, please delete it and run again')
-    except KeyboardInterrupt: # When thread fails because subprocess command threw some error/exception
+        logging.error(e)
+    except KeyboardInterrupt as e: # When thread fails because subprocess command threw some error/exception
         print()
         print('ERROR: Something happened during the cloning process; your repos are not at the proper timestamp. Delete the assignment folder and run again.')
-    except:
-        print(f'Cloned {len(next(os.walk(initial_path))[1])}/{len(repos)} repos')
+        logging.error(e)
+    except ValueError as e: # When git version is incompatible w/ script
+        print()
+        print(f'ERROR: Your version of git is not compatible with this script. Use version {MIN_GIT_VERSION}+.')
+        logging.error(e)
+    except Exception as e:
+        print(f'ERROR: Something happened. Check {LOG_FILE_PATH}')
+        logging.error(e)
     exit()
 
 
